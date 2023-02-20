b8d69e25023ab57bd503049e1a5efeab0f68d7b4
Elfin Git <musongoletracy@gmail.com>
2023-02-20 23:40:41 +0200

    update project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,17 +3,16 @@
 import { Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-const ProjectCard = ({ title, description, imgUrl, liveLink, sourceLink }) => {
+const ProjectCard = ({ title, description, imgUrl, liveLink }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} alt="pic 1" className="pr-pic" />
         <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+          <span>{title}</span>
           <div>
             <button type="button" className="btn-link" onClick={() => window.open(liveLink)}>See live</button>
-            <button type="button" className="btn-link" onClick={() => window.open(sourceLink)}>See source</button>
+            <button type="button" className="btn-link" onClick={() => window.open(description)}>See source</button>
           </div>
         </div>
       </div>
@@ -26,7 +25,6 @@ ProjectCard.propTypes = {
   description: PropTypes.string.isRequired,
   imgUrl: PropTypes.string.isRequired,
   liveLink: PropTypes.string.isRequired,
-  sourceLink: PropTypes.string.isRequired,
 };
 
 export default ProjectCard;
